4e22affecd3432b10d40817862e1ec266b74fc00
syzspectroom <syzspectroom@gmail.com>
2010-11-28 13:13:32 +0200

    sceleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podii::Application.config.secret_token = '82d734caaf2a5557349d63674995cc6b0c201d6cd4c6d308d78b282539b6618728b1b8adfe07bd8f7e1a1c22e83a0f1499391351fdcaf7c55c80c0bd2cba2d04'
